83437955c6c3446b9a8f2d173e7cc7ab04d433c9
Evgenii Orel <evgenii_orel@epam.com>
2018-11-22 18:46:53 +0100

    initial commit

---
+++ spec/atom-micromanager-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomMicromanagerView from '../lib/atom-micromanager-view';
+
+describe('AtomMicromanagerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
